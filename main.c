b1bd1ffa2cb89d7fea2a40dfb7342138353ae9f8
VITHOR FÉLIX JALES MIRANDA <vithor.miranda@a.ucb.br>
2025-04-23 09:57:13 -0300

    Correcao dos arquivos iniciais

--- main.c
+++ main.c
@@ -5,19 +5,25 @@
 #define COL 100
 
 void gerarGrid(int porcentagemObstaculos);
-void salvarGridNoTxt(char grid[][]);
+void salvarGridNoTxt(char * grid[]);
 
 int main() {
-    int porcentagem = 25;
+    int porcentagem;
     puts("Boas vindas ao sistema de navegacao para drones autonomos.");
-    printf("Para gerar o mapa, informe a porcentagem de obstaculos no caminho:\n\t> ");
-    scanf("%d", &porcentagem);  
+    printf("Para gerar o mapa, informe a porcentagem de obstaculos no caminho:\n");
+    do {
+        printf("\t >");
+        scanf("%d", &porcentagem);
+    } while (porcentagem < 0 || porcentagem > 100);
     puts("Gerando o mapa...");
-    sleep(2):
+    Sleep(3000);
+    gerarGrid(porcentagem);
 }
 
 void gerarGrid(int porcentagemObstaculos) {
-
+    char grid[LIN][COL] = {{' '}};
+    int qntdObstaculos = (LIN*COL) * (porcentagemObstaculos / 100);
+    printf("%d", qntdObstaculos);
 }
 
-void salvarGridNoTxt(char grid[][]);
+void salvarGridNoTxt(char * grid[]);
